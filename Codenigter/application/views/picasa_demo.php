b694e6d4ce9f68da4f8db8a9384cebd191c2a659
Soyo Solution <xenialaw418@gmail.com>
2014-06-20 09:45:03 +0800

    Update picasa_demo.php
    Add the original image for compare

--- Codenigter/application/views/picasa_demo.php
+++ Codenigter/application/views/picasa_demo.php
@@ -1,1 +1,6 @@
-<img src="<?php echo $newurl; ?>" alt="demo img"/>
+Original Image:<br />
+<img src="<?php echo $oldurl; ?>" alt="orginal img"/><br />
+<br /><br />
+Image with new size:<br />
+<img src="<?php echo $newurl; ?>" alt="demo img"/><br />
+<br /><br />
