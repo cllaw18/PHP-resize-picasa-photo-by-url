462f56dffd44c3baa29d4d3f2226b6b1d942feef
Soyo Solution <xenialaw418@gmail.com>
2014-06-20 09:46:47 +0800

    Update picasa.php
    Change the another image for demo. Add a paras to pass the original image to view.

--- Codenigter/application/controllers/picasa.php
+++ Codenigter/application/controllers/picasa.php
@@ -9,12 +9,13 @@ class picasa extends CI_Controller {
 
 	public function index()
 	{
-        $url = "https://lh4.googleusercontent.com/-dAejSnYrNw8/U6KpRsDbkNI/AAAAAAAAAyo/FetX2d5CN_A/s0/53a2a944b1842.jpg";
-        $imgsize = "400";
+        $url = "https://lh3.googleusercontent.com/-0xxGDL4nB5k/U6ORIvopgsI/AAAAAAAABJY/5VXqVws8a-4/s512/computer.png";
+        $imgsize = "100";
         $this->load->library('picasa_resize');
+        $data['oldurl'] = $url;
         $data['newurl'] = $this->picasa_resize->resize_image($url, $imgsize);
         //$data['newurl'] = $this->picasa_resize->resize_image("https://lh4.googleusercontent.com/-dAejSnYrNw8/U6KpRsDbkNI/AAAAAAAAAyo/FetX2d5CN_A/s0/53a2a944b1842.jpg", "400");
-		$this->load->view('index', $data);
+		$this->load->view('picasa_demo', $data);
 	}//EOF index()
     
 }
